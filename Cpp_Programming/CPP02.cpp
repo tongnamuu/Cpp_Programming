f10df5a24160efb58ef6ac8c49712f6d64c66570
tongnamuu <thban33@naver.com>
2020-05-12 03:06:13 +0900

    Output Formatting 1

--- Cpp_Programming/CPP02.cpp
+++ Cpp_Programming/CPP02.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main() {
     cout << "We don't have to use std:: Use\n"
-	    << setw(51) << right << "using name space std" << endl;
+        << setw(51) << right << "using name space std" << endl;
     return 0;
 } 
